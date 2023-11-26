72c9486caf98876b60c9648fa0ee91e6bab0dfec
szekelyisti <szekelyisti@gmail.com>
2023-11-26 21:27:54 +0100

    add pdf generation to endpoint

--- src/main/java/org/ppke/itk/groupmealplanner/controller/MealController.java
+++ src/main/java/org/ppke/itk/groupmealplanner/controller/MealController.java
@@ -2,10 +2,21 @@ package org.ppke.itk.groupmealplanner.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.ppke.itk.groupmealplanner.domain.Meal;
 import org.ppke.itk.groupmealplanner.repository.mocks.MockMealRepository;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_PDF_VALUE;
@@ -31,7 +42,40 @@ public class MealController {
         return mealRepository.findById(id);
     }
 
-    @GetMapping(value = "/meals/{id}", produces =APPLICATION_PDF_VALUE)
+    @GetMapping(value = "/meals/{id}", produces = APPLICATION_PDF_VALUE)
+    public ResponseEntity<InputStreamResource> getMealByIdAsPDF(@PathVariable("id") Integer id) throws IOException {
+        Meal meal = mealRepository.findById(id);
+
+        PDDocument document = new PDDocument();
+        PDPage page = new PDPage();
+        document.addPage(page);
+
+        PDPageContentStream contentStream = new PDPageContentStream(document, page);
+
+        contentStream.beginText();
+        contentStream.setFont(PDType1Font.TIMES_ROMAN, 12);
+        contentStream.newLineAtOffset(25, 500);
+        contentStream.showText("Meal Name: " + meal.getName());
+        contentStream.newLine();
+        contentStream.showText("Meal Instructions: " + meal.getInstructions());
+        contentStream.newLine();
+        contentStream.showText("Meal Approximated Price: " + meal.getApproximatedPrice());
+        contentStream.endText();
+        contentStream.close();
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        document.save(out);
+        document.close();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "inline; filename=meal-" + id + ".pdf");
+
+        return ResponseEntity
+                .ok()
+                .headers(headers)
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(new InputStreamResource(new ByteArrayInputStream(out.toByteArray())));
+    }
 
     @PostMapping("/meals")
     public Meal createMeal(@RequestBody String name, @RequestBody String instructions, @RequestBody Integer approximatedPrice) {
