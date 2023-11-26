ec7484509ca141913ebe0fedb3d1c2d577403160
szekelyisti <szekelyisti@gmail.com>
2023-11-26 15:02:35 +0100

    initial commit

---
+++ src/main/java/org/ppke/itk/groupmealplanner/GroupMealPlannerApplication.java
@@ -0,0 +1,13 @@
+package org.ppke.itk.groupmealplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupMealPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupMealPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ppke/itk/groupmealplanner/GroupMealPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ppke.itk.groupmealplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupMealPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
